df1c5827f1b0bfa2d522fb7411d74694850d39b1
codiku-dev <111140836+codiku-dev@users.noreply.github.com>
2023-04-07 10:19:58 +0200

    wip

--- App.js
+++ App.js
@@ -1,5 +1,19 @@
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
-
+import { Text, View, ImageBackground } from "react-native";
+import hotBackground from "./assets/hot.png";
 export default function App() {
-  return <></>;
+  return (
+    <ImageBackground style={s.backgroundImg} source={hotBackground}>
+      <SafeAreaProvider>
+        <SafeAreaView style={s.root}>
+          <View style={s.workspace}>
+            <Text>Temperature</Text>
+            <Text>Input</Text>
+            <Text>Button</Text>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ImageBackground>
+  );
 }
--- App.style.js
+++ App.style.js
@@ -1,3 +1,16 @@
 import { StyleSheet } from "react-native";
 
-export const s = StyleSheet.create({});
+export const s = StyleSheet.create({
+  root: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  workspace: {
+    height: 450,
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  backgroundImg: {
+    height: "100%",
+  },
+});
